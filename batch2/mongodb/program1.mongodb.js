e4635ada955fafa4a062d58a98e6b1c751e672b8
richieArnold <arnoldrichie31.rar@gmail.com>
2025-03-17 20:39:25 +0530

    add cars in car-rental

--- batch2/mongodb/program1.mongodb.js
+++ batch2/mongodb/program1.mongodb.js
@@ -11,53 +11,65 @@ db.createCollection("cars");
 //   rental_status: "available",
 // });
 
-db.cars.insertMany([
-  {
-    _id: "CarTY02",
-    make: "Toyota",
-    model: "Corolla",
-    year: 2018,
-    price: 18000,
-    rental_status: "available",
-  },
-  {
-    _id: "CarHO03",
-    make: "Honda",
-    model: "Civic",
-    year: 2019,
-    price: 20000,
-    rental_status: "available",
-  },
-  {
-    _id: "CarTY04",
-    make: "Toyota",
-    model: "Etios",
-    year: 2017,
-    price: 16000,
-    rental_status: "available",
-  },
-  {
-    _id: "CarHO05",
-    make: "Honda",
-    model: "Accord",
-    year: 2019,
-    price: 25000,
-    rental_status: "available",
-  },
-  {
-    _id: "CarVW06",
-    make: "Volkswagen",
-    model: "Tiguan",
-    year: 2019,
-    price: 30000,
-    rental_status: "available",
-  },
-  {
-    _id: "CarVW07",
-    make: "Volkswagen",
-    model: "Taigun",
-    year: 2019,
-    price: 30000,
-    rental_status: "available",
-  },
-]);
+// db.cars.insertMany([
+//   {
+//     _id: "CarTY02",
+//     make: "Toyota",
+//     model: "Corolla",
+//     year: 2018,
+//     price: 18000,
+//     rental_status: "available",
+//   },
+//   {
+//     _id: "CarHO03",
+//     make: "Honda",
+//     model: "Civic",
+//     year: 2019,
+//     price: 20000,
+//     rental_status: "available",
+//   },
+//   {
+//     _id: "CarTY04",
+//     make: "Toyota",
+//     model: "Etios",
+//     year: 2017,
+//     price: 16000,
+//     rental_status: "available",
+//   },
+//   {
+//     _id: "CarHO05",
+//     make: "Honda",
+//     model: "Accord",
+//     year: 2019,
+//     price: 25000,
+//     rental_status: "available",
+//   },
+//   {
+//     _id: "CarVW06",
+//     make: "Volkswagen",
+//     model: "Tiguan",
+//     year: 2019,
+//     price: 30000,
+//     rental_status: "available",
+//   },
+//   {
+//     _id: "CarVW07",
+//     make: "Volkswagen",
+//     model: "Taigun",
+//     year: 2019,
+//     price: 30000,
+//     rental_status: "available",
+//   },
+// ]);
+
+// fetch all the cars in the db
+db.cars.find();
+
+// fetch all the cars made my honda
+// db.cars.find({ make: "Honda" });
+
+// fetch all the cars made my honda and volkswagen
+// db.cars.find({ make: { $in: ["Honda", "Volkswagen"] } });
+
+//update the price of the car CarHO03 to 22000
+db.cars.updateOne({ _id: "CarHO03" }, { $set: { price: 22000 } });
