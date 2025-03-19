085766eaf5b1166ea3dcad65ae5d0b4aa11ad5a6
richieArnold <arnoldrichie31.rar@gmail.com>
2025-03-19 20:50:32 +0530

    add aggregation pipeline

---
+++ batch2/mongodb/program4.mongodb.js
@@ -0,0 +1,45 @@
+// fetch details of all bookings along with the customer details
+
+use("car-rentals");
+
+//fetch details of booking booking_03
+db.bookings.aggregate([
+  {
+    $match: {
+      _id: "booking_03",
+    },
+  },
+  {
+    $lookup: {
+      from: "users",
+      localField: "customer_id",
+      foreignField: "_id",
+      as: "customer_details",
+    },
+  },
+  {
+    $lookup: {
+      from: "cars",
+      localField: "car_id",
+      foreignField: "_id",
+      as: "car_details",
+    },
+  },
+  {
+    $unwind: "$customer_details",
+  },
+  {
+    $unwind: "$car_details",
+  },
+  {
+    $project: {
+      _id: 1,
+      customer_name: "$customer_details.name",
+      customer_email: "$customer_details.email",
+      customer_contact: "$customer_details.contact",
+      car_model: "$car_details.model",
+      car_brand: "$car_details.brand",
+      car_price: "$car_details.price",
+    },
+  },
+]);
