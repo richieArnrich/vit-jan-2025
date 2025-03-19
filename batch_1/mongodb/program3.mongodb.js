1d0ea631d0fa59ce4850ef1f4dc0acef699dde23
richieArnold <arnoldrichie31.rar@gmail.com>
2025-03-19 19:25:32 +0530

    add aggregation pipeline

--- batch_1/mongodb/program3.mongodb.js
+++ batch_1/mongodb/program3.mongodb.js
@@ -8,15 +8,32 @@ db.createCollection("bookings");
 //   bike_id: "bikeH01",
 // });
 
-db.bookings.insertMany([
-  {
-    _id: "booking_02",
-    customer_id: "cust_02",
-    bike_id: "bikeT03",
-  },
-  {
-    _id: "booking_03",
-    customer_id: "cust_03",
-    bike_id: "bikeRE04",
-  },
-]);
+// db.bookings.insertMany([
+//   {
+//     _id: "booking_02",
+//     customer_id: "cust_02",
+//     bike_id: "bikeT03",
+//   },
+//   {
+//     _id: "booking_03",
+//     customer_id: "cust_03",
+//     bike_id: "bikeRE04",
+//   },
+// ]);
+
+// db.bookings.insertOne({
+//   _id: "booking_04",
+//   customer_id: "cust_03",
+//   bike_id: "bikeH01",
+// });
+
+// db.bookings.insertOne({
+//   _id: "booking_05",
+//   customer_id: "cust_02",
+//   bike_id: "bikeRE04",
+// });
+
+db.bookings.find();
+
+// find data of booking_id booking_03
+// db.bookings.find({ _id: "booking_03" });
