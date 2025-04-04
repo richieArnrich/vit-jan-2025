88f9ca1bc97f821b1d28d444995fb55f3e5ef484
richieArnold <arnoldrichie31.rar@gmail.com>
2025-04-04 20:41:01 +0530

    add reguser

--- batch2/crud-app/server/controllers/userControllers.js
+++ batch2/crud-app/server/controllers/userControllers.js
@@ -3,15 +3,29 @@
 const express = require("express");
 const router = express.Router();
 
-const user = require("../models/userSchema.js");
+const User = require("../models/userSchema.js");
 
 router.get("/allusers", async (req, res) => {
   try {
-    const userData = await user.find();
+    const userData = await User.find();
     res.json(userData);
   } catch (err) {
     console.log(err);
     res.json({ message: "Error fetching users" });
+  }
+});
+
+router.post("/regUser", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { name, email, password, contact } = req.body;
+    // new record in db
+    const user = new User({ name, email, password, contact });
+    await user.save();
+    res.json({ message: "user registered successfully", userDoc: user });
+  } catch (err) {
+    console.log(err);
+    res.json({ message: "Error registering user" });
   }
 });
 
